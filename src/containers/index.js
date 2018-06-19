abad05e0d42abed7ab1646e013fde3d99f74630e
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-06-19 11:04:24 +0800

    initial structure

---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Game from './Game/Game';
+import './index.css';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Game />
+      </div>
+    );
+  }
+}

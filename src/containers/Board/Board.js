4b3f7d94b45a4aee64dae246d802266aaeb11852
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-06-19 16:30:42 +0800

    玩家輪流

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -4,13 +4,25 @@ import Square from '../Square/Square';
 import './Board.css';
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext === true ? 'X' : 'O';
+    this.setState({ squares, xIsNext: !this.state.xIsNext });
+  }
 
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
